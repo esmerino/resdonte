a848954bb050cafcb2befabe55382d5ffa79ee3b
José Esmerino <jose.esmerino@gmail.com>
2023-09-18 16:48:35 -0300

    Adding .env

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token =  Rails.application.credentials.openai.access_token
-  config.organization_id = Rails.application.credentials.openai.organization_id
+  config.access_token = ENV["OPENAI_ACCESS_TOKEN"] || Rails.application.credentials.openai.access_token
+  config.organization_id = ENV["OPENAI_ORGANIZATION_ID"] || Rails.application.credentials.openai.organization_id
 end
